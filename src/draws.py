7c61a9bcde363085d1baee15f1268dfbbc69cff1
tim <tim@ellemeeet.nl>
2019-06-26 13:40:53 +0000

    more results

--- src/draws.py
+++ src/draws.py
@@ -8,19 +8,6 @@ def makedraws(drawtype, n_r, n_q, n_k, verbose):
         if drawtype == 'pseudo':
             draws = np.random.randn(n_r, n_q, n_k);
         elif drawtype == 'halton':
-            
-#             #initialize sequencer with K dimensions
-#             sequencer = ghalton.Halton(n_k)
-            
-#             #make the draws and discard the 10 inital values to prevent issues
-#             draws = sequencer.get(n_q * n_r + 10)[10:]
-            
-#             #convert draws to standard variates by inverse cdf
-#             draws = norm.ppf(draws)
-            
-#             #shape the draws into the right shape
-#             draws = np.array(np.split(draws, n_r))
-                
             #initialize sequencer with K dimensions
             sequencer = ghalton.Halton(n_k)
             
